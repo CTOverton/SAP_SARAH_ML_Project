f4ec98247590f5ff59a110b2a7444460552fda01
CTOverton <C@CTOverton.com>
2018-06-13 12:30:38 -0400

    Shelf UI More or less complete

--- Shelf UI/js/index.js
+++ Shelf UI/js/index.js
@@ -1,9 +1,13 @@
-var reds, blues, whites, empty, missing = 0;
+var reds, whites, blues, emptys, missings = 0;
 var slotValues = [
-    ["A","B","C"],
-    ["D","E","F"],
-    ["G","H","I"]
+    ["R","W","B"],
+    ["R","E","W"],
+    ["W","B","M"]
 ];
+window.onload = function(){
+    updateTotals();
+    updateSlot();
+};
 
 function updateUI() {
     var form = document.getElementById("sampleDataForm");
@@ -25,36 +29,89 @@ function updateUI() {
         [slot_20, slot_21, slot_22],
     ];
 
+    updateSlot();
+    updateTotals();
+}
+
+function updateTotals() {
+    reds = whites = blues = emptys = missings = 0;
+
+    for (var i = 0; i < slotValues.length; i++) { // Loop for rows
+        for (var j = 0; j < slotValues[i].length; j++) { // Loop for cols
+            switch (slotValues[i][j].toLowerCase()){
+                case "r":
+                    reds += 1;
+                    break;
+                case "w":
+                    whites += 1;
+                    break;
+                case "b":
+                    blues += 1;
+                    break;
+                case "e":
+                    emptys += 1;
+                    break;
+                default:
+                    missings += 1;
+            }
+        }
+    }
+
+    var totals = [reds, whites, blues, emptys, missings];
+    var max = 0;
+
+    for (var x = 0; x < totals.length; x++){
+        if (max < totals[x]){max = totals[x];}
+    }
+
+
+    document.getElementsByClassName("totals-bar red")[0].style.width = ((reds/max)*100) + "%";
+    document.getElementsByClassName("totals-bar red")[0].innerHTML = "<p>" + reds + "</p>";
+
+    document.getElementsByClassName("totals-bar white")[0].style.width = ((whites/max)*100) + "%";
+    document.getElementsByClassName("totals-bar white")[0].innerHTML = "<p>" + whites + "</p>";
+
+    document.getElementsByClassName("totals-bar blue")[0].style.width = ((blues/max)*100) + "%";
+    document.getElementsByClassName("totals-bar blue")[0].innerHTML = "<p>" + blues + "</p>";
+
+    document.getElementsByClassName("totals-bar empty")[0].style.width = ((emptys/max)*100) + "%";
+    document.getElementsByClassName("totals-bar empty")[0].innerHTML = "<p>" + emptys + "</p>";
+
+    document.getElementsByClassName("totals-bar missing")[0].style.width = ((missings/max)*100) + "%";
+    document.getElementsByClassName("totals-bar missing")[0].innerHTML = "<p>" + missings + "</p>";
+}
+
+function updateSlot() {
+
     var gridChildren = document.getElementById("grid-container").children;
 
     for (var i = 0; i < gridChildren.length; i++) {
         var rowChildren = gridChildren[i].children;
         for (var j = 0; j < rowChildren.length; j++) {
-            updateSlot(rowChildren[j], slotValues[i][j]);
-        }
-    }
-}
+            slot = rowChildren[j];
+            slotValue = slotValues[i][j];
 
-function updateSlot(slot, slotValue) {
-    switch (slotValue.toLowerCase()){
-        case "r":
-            slot.innerHTML = "<p class=\"label\">R</p>";
-            slot.className = "grid-cell red";
-            break;
-        case "w":
-            slot.innerHTML = "<p class=\"label\">W</p>";
-            slot.className = "grid-cell white";
-            break;
-        case "b":
-            slot.innerHTML = "<p class=\"label\">B</p>";
-            slot.className = "grid-cell blue";
-            break;
-        case "e":
-            slot.innerHTML = "";
-            slot.className = "grid-cell empty";
-            break;
-        default:
-            slot.innerHTML = "";
-            slot.className = "grid-cell missing";
+            switch (slotValue.toLowerCase()){
+                case "r":
+                    slot.innerHTML = "<p class=\"label\">R</p>";
+                    slot.className = "grid-cell red";
+                    break;
+                case "w":
+                    slot.innerHTML = "<p class=\"label\">W</p>";
+                    slot.className = "grid-cell white";
+                    break;
+                case "b":
+                    slot.innerHTML = "<p class=\"label\">B</p>";
+                    slot.className = "grid-cell blue";
+                    break;
+                case "e":
+                    slot.innerHTML = "";
+                    slot.className = "grid-cell empty";
+                    break;
+                default:
+                    slot.innerHTML = "";
+                    slot.className = "grid-cell missing";
+            }
+        }
     }
 }
