00bd339c1e3dfe861852b1438e66cb636a1d5eb0
CTOverton <C@CTOverton.com>
2018-06-15 09:50:43 -0400

    Shelf UI Best Practices

--- Shelf UI/js/index.js
+++ Shelf UI/js/index.js
@@ -21,25 +21,12 @@ function randomizeData() {
     updateUI();
 }
 
-function updateSampleData() {
-    var form = document.getElementById("sampleDataForm");
-    var slot_00 = form.elements["slot_00"].value;
-    var slot_01 = form.elements["slot_01"].value;
-    var slot_02 = form.elements["slot_02"].value;
-
-    var slot_10 = form.elements["slot_10"].value;
-    var slot_11 = form.elements["slot_11"].value;
-    var slot_12 = form.elements["slot_12"].value;
-
-    var slot_20 = form.elements["slot_20"].value;
-    var slot_21 = form.elements["slot_21"].value;
-    var slot_22 = form.elements["slot_22"].value;
-
-    slotValues = [
-        [slot_00, slot_01, slot_02],
-        [slot_10, slot_11, slot_12],
-        [slot_20, slot_21, slot_22],
-    ];
+function updateSampleData(values) {
+    for (var i = 0; i < slotValues.length; i++) {
+        for (var j = 0; j < slotValues.length; j++) {
+            slotValues[i][j] = values[i][j];
+        }
+    }
 
     updateUI();
 }
