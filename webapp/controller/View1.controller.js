4c4e7a5375e1025f7a169f81bacc540773698573
CTOverton <christianoverton@ctoverton.com>
2018-06-19 16:25:12 +0000

    XML App start

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,9 +1,21 @@
+var that;
+
 sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function(Controller) {
 	"use strict";
 
 	return Controller.extend("SAP_SARAH_ML_Project.controller.View1", {
-
+		onAfterRendering: function() {
+			that = this;
+			
+			var listItems;
+			var items = that.getView().byId("__list0");
+			console.log(items);
+			// for(var i; i < items.length; i++) {
+			// 	listItems[i] = items[i];
+			// }
+			
+		}
 	});
 });
